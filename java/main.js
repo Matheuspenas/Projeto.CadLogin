a3e1387275885e60cebc8159b64b095c151e40cf
matheuspenas <mathegupena@gmail.com>
2025-04-11 21:34:59 -0300

    adição da funcionalidade armazenamento de dados dentro de uma tabela

--- java/main.js
+++ java/main.js
@@ -9,3 +9,19 @@ function acessar(){
         window.location.href = 'cadrastro.html';
     }
 }
+
+// declara Array para o projeto
+
+var dadosLista = [];
+// função de aramzenamento de nomes em Array
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log.dadosLista;
+        nomeUser ="";
+    } else{
+        alert("Usuario não preenchido");
+    }
+}
